0897f0944b983cf996743fe82f2736f6bb055d8f
jan vitek <janvite@yahoo.com>
2016-03-05 09:49:35 -0500

    Not sure what this was...
    I should have pushed it a while ago.

--- src/main/java/neu/perf/Job.java
+++ src/main/java/neu/perf/Job.java
@@ -26,143 +26,137 @@ import com.amazonaws.services.elasticmapreduce.model.StepSummary;
  * @author Joe Sackett, maintainer Jan Vitek
  */
 public abstract class Job {
-  App.Conf conf; // the configuration of AWS
-  Vector<Integer> seconds = new Vector<Integer>(); // execution time in seconds
+	App.Conf conf; // the configuration of AWS
+	Vector<Integer> seconds = new Vector<Integer>(); // execution time in seconds
 
-  public static Job build(App.Conf conf) {
-    if (conf.kind.equals("plain")) return new Plain(conf);
-    else if (conf.kind.equals("local")) return new Local(conf);
-    else if (conf.kind.equals("cloud")) return new Cloud(conf);
-    else throw new Error("Specify job kind");
-  }
+	public static Job build(App.Conf conf) {
+		if (conf.kind.equals("plain")) return new Plain(conf);
+		else if (conf.kind.equals("local")) return new Local(conf);
+		else if (conf.kind.equals("cloud")) return new Cloud(conf);
+		else throw new Error("Specify job kind");
+	}
 
-  Job(App.Conf conf) {
-    this.conf = conf;
-  }
+	Job(App.Conf conf) {
+		this.conf = conf;
+	}
 
-  public abstract void run() throws Exception;
+	public abstract void run() throws Exception;
 
-  public void finish() {}
+	public void finish() {}
 
-  public Integer[] getResults() {
-    return seconds.toArray(new Integer[0]);
-  }
+	public Integer[] getResults() {
+		return seconds.toArray(new Integer[0]);
+	}
 
-  public int timeExec(ProcessBuilder builder) throws Exception {
-    App.Executor proc = new App.Executor(builder);
-    Timer time = new Timer();
-    proc.execute();
-    time.stop();
-    return time.seconds();
-  }
+	public int timeExec(ProcessBuilder builder) throws Exception {
+		App.Executor proc = new App.Executor(builder);
+		Timer time = new Timer();
+		proc.execute();
+		time.stop();
+		return time.seconds();
+	}
 
-  String[] concat(String[] preArgs, String[] args) {
-    return (String[]) ArrayUtils.addAll(preArgs, args);
-  }
+	String[] concat(String[] preArgs, String[] args) {
+		return (String[]) ArrayUtils.addAll(preArgs, args);
+	}
 
-  /**
-   * A Java task running locally without Hadoop.
-   */
-  static class Plain extends Job {
+	/**
+	 * A Java task running locally without Hadoop.
+	 */
+	static class Plain extends Job {
+		Plain(App.Conf conf) {
+			super(conf);
+		}
 
-    Plain(App.Conf conf) {
-      super(conf);
-    }
+		@Override
+		public void run() throws Exception {
+			seconds.add(timeExec(new ProcessBuilder(
+					concat(new String[] { "java", "-cp", conf.jar, conf.main, "-input=" + conf.input, "-output=" + conf.output },
+							conf.arguments))));
+		}
+	}
 
-    @Override
-    public void run() throws Exception {
-      seconds.add(timeExec(new ProcessBuilder(
-          concat(new String[] { "java", "-cp", conf.jar, conf.main, "-input=" + conf.input, "-output=" + conf.output },
-              conf.arguments))));
-    }
-  }
+	/**
+	 * A local pseudo-distributed task running with Hadoop.
+	 */
+	static class Local extends Job {
+		Local(App.Conf conf) {
+			super(conf);
+		}
 
-  /**
-   * A local pseudo-distributed task running with Hadoop.
-   */
-  static class Local extends Job {
+		@Override
+		public void run() throws Exception {
+			ProcessBuilder pb = new ProcessBuilder(concat(new String[] { conf.hadoopHome + "/bin/hadoop", "jar", conf.jar, conf.main,
+					"-input=" + conf.input, "-output=" + conf.output }, conf.arguments));
+			deleteDirectory(new File(conf.output));
+			seconds.add(timeExec(pb));
+		}
 
-    Local(App.Conf conf) {
-      super(conf);
-    }
+		private static boolean deleteDirectory(File dir) {
+			if (!dir.exists() || !dir.isDirectory()) return false;
+			File[] files = dir.listFiles();
+			if (files != null) for (File file : files)
+				if (file.isDirectory()) deleteDirectory(file);
+				else file.delete();
+			return (dir.delete());
+		}
+	}
 
-    @Override
-    public void run() throws Exception {
-      ProcessBuilder pb = new ProcessBuilder(concat(new String[] { conf.hadoopHome + "/bin/hadoop", "jar", conf.jar, conf.main,
-          "-input=" + conf.input, "-output=" + conf.output }, conf.arguments));
-      deleteDirectory(new File(conf.output));
-      seconds.add(timeExec(pb));
-    }
+	/**
+	 * A task running on AWS.
+	 */
+	static class Cloud extends Job {
+		AmazonElasticMapReduce client = new AmazonElasticMapReduceClient(new ProfileCredentialsProvider().getCredentials());
+		List<StepConfig> steps = new LinkedList<StepConfig>();
+		int num;
+		String clusterId;
 
-    private static boolean deleteDirectory(File dir) {
-      if (!dir.exists() || !dir.isDirectory()) return false;
-      File[] files = dir.listFiles();
-      if (files != null) for (File file : files)
-        if (file.isDirectory()) deleteDirectory(file);
-        else file.delete();
-      return (dir.delete());
-    }
-  }
+		Cloud(App.Conf conf) {
+			super(conf);
+		}
 
-  /**
-   * A task running on AWS.
-   */
-  static class Cloud extends Job {
+		@Override
+		public void run() throws Exception {
+			HadoopJarStepConfig hc = new HadoopJarStepConfig().withJar(conf.jar).withMainClass(conf.main)
+					.withArgs(concat(new String[] { "-input=" + conf.input, "-output=" + conf.output }, conf.arguments));
+			steps.add(new StepConfig(conf.stepName + " " + num, hc));
+			num++;
+		}
 
-    AmazonElasticMapReduce client = new AmazonElasticMapReduceClient(new ProfileCredentialsProvider().getCredentials());
-    List<StepConfig> steps = new LinkedList<StepConfig>();
-    int num;
-    String clusterId;
+		@Override
+		public void finish() {
+			RunJobFlowRequest request = new RunJobFlowRequest().withName(conf.clusterName).withReleaseLabel(conf.releaseLabel)
+					.withSteps(steps.toArray(new StepConfig[0])).withLogUri(conf.logUri).withServiceRole(conf.serviceRole)
+					.withJobFlowRole(conf.jobFlowRole)
+					.withInstances(new JobFlowInstancesConfig().withEc2KeyName(conf.ec2KeyName).withInstanceCount(conf.instanceCount)
+							.withKeepJobFlowAliveWhenNoSteps(conf.keepJobFlowAlive).withMasterInstanceType(conf.masterInstance)
+							.withSlaveInstanceType(conf.slaveInstance));
+			RunJobFlowResult res = client.runJobFlow(request);
+			clusterId = res.getJobFlowId();
+			DescribeClusterRequest clusterReq = new DescribeClusterRequest().withClusterId(clusterId);
+			while (true) {
+				DescribeClusterResult cres = client.describeCluster(clusterReq);
+				String state = cres.getCluster().getStatus().getState();
+				if (state.equals(ClusterState.TERMINATED.name()) || state.equals(ClusterState.TERMINATED_WITH_ERRORS.name())
+						|| state.equals(ClusterState.TERMINATING.name())) {
+					break;
+				}
+				try {
+					Thread.sleep(10 * 1000);
+				} catch (InterruptedException e) {}
+			}
+		}
 
-    Cloud(App.Conf conf) {
-      super(conf);
-    }
-
-    @Override
-    public void run() throws Exception { // Hadoop program step.
-      HadoopJarStepConfig hc = new HadoopJarStepConfig().withJar(conf.jar).withMainClass(conf.main)
-          .withArgs(concat(new String[] { "-input=" + conf.input, "-output=" + conf.output }, conf.arguments));
-      steps.add(new StepConfig(conf.stepName + " " + num, hc));
-      num++;
-    }
-
-    @Override
-    public void finish() {
-      RunJobFlowRequest request = new RunJobFlowRequest().withName(conf.clusterName).withReleaseLabel(conf.releaseLabel)
-          .withSteps(steps.toArray(new StepConfig[0])).withLogUri(conf.logUri).withServiceRole(conf.serviceRole)
-          .withJobFlowRole(conf.jobFlowRole)
-          .withInstances(new JobFlowInstancesConfig().withEc2KeyName(conf.ec2KeyName).withInstanceCount(conf.instanceCount)
-              .withKeepJobFlowAliveWhenNoSteps(conf.keepJobFlowAlive).withMasterInstanceType(conf.masterInstance)
-              .withSlaveInstanceType(conf.slaveInstance));
-
-      RunJobFlowResult res = client.runJobFlow(request);
-      clusterId = res.getJobFlowId();
-
-      DescribeClusterRequest clusterReq = new DescribeClusterRequest().withClusterId(clusterId);
-
-      while (true) {
-        DescribeClusterResult cres = client.describeCluster(clusterReq);
-        String state = cres.getCluster().getStatus().getState();
-        if (state.equals(ClusterState.TERMINATED.name()) || state.equals(ClusterState.TERMINATED_WITH_ERRORS.name())
-            || state.equals(ClusterState.TERMINATING.name())) {
-          break;
-        }
-        try {
-          Thread.sleep(10 * 1000);
-        } catch (InterruptedException e) {}
-      }
-    }
-
-    @Override
-    public Integer[] getResults() {
-      ListStepsRequest req = new ListStepsRequest().withClusterId(clusterId);
-      ListStepsResult res = client.listSteps(req);
-      for (StepSummary sum : res.getSteps()) {
-        long time = sum.getStatus().getTimeline().getEndDateTime().getTime()
-            - sum.getStatus().getTimeline().getStartDateTime().getTime();
-        seconds.add((int) (time / 1000));
-      }
-      return seconds.toArray(new Integer[0]);
-    }
-  }
+		@Override
+		public Integer[] getResults() {
+			ListStepsRequest req = new ListStepsRequest().withClusterId(clusterId);
+			ListStepsResult res = client.listSteps(req);
+			for (StepSummary sum : res.getSteps()) {
+				long time = sum.getStatus().getTimeline().getEndDateTime().getTime()
+						- sum.getStatus().getTimeline().getStartDateTime().getTime();
+				seconds.add((int) (time / 1000));
+			}
+			return seconds.toArray(new Integer[0]);
+		}
+	}
 }
